3a7cbb2c62890e82e62dba3536eb99960dc8c878
Alex Naidis <alex.naidis@linux.com>
2017-04-28 19:54:13 +0200

    media: Allow using legacy HSR calculation
    000d415 from MR2 is incompatible with pre-MR2 Qualcomm camera binaries.
    
    Any device with said binaries before needs to opt in to
    TARGET_HAS_LEGACY_HSR to have working High Speed Recording
    (slow motion video).
    
    Change-Id: I6904a9993407d4911e27c1d5fe0851e2ddbd58e8
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -165,6 +165,10 @@ ifeq ($(TARGET_OMX_LEGACY_RESCALING),true)
 LOCAL_CFLAGS += -DUSE_LEGACY_RESCALING
 endif
 
+ifeq ($(TARGET_HAS_LEGACY_HSR),true)
+LOCAL_CFLAGS += -DLEGACY_HSR
+endif
+
 LOCAL_CFLAGS += -Wno-multichar -Werror -Wno-error=deprecated-declarations -Wall
 
 LOCAL_C_INCLUDES += $(call project-path-for,qcom-media)/mm-core/inc
