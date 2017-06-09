d89d4d3dd1493d942ed6eb5272076f936e734d9f
zengjing <zengjing@xiaomi.com>
2017-06-09 08:32:34 +0800

    fix deadlock upon create Effect fail
    AudioFlinger::ThreadBase::createEffect_l() should not hold mLock on failure
    and clearing handle as the destructor of AudioFlinger::EffectHandle
    will call AudioFlinger::ThreadBase::disconnectEffectHandle, which will
    also try to lock mLock.
    
    Test: N/A
    
    Change-Id: I45ed13337f11429990ce361f4649704c660b3e1e
    Signed-off-by: zengjing <zengjing@xiaomi.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -1345,15 +1345,18 @@ sp<AudioFlinger::EffectHandle> AudioFlinger::ThreadBase::createEffect_l(
 Exit:
     if (lStatus != NO_ERROR && lStatus != ALREADY_EXISTS) {
 
-        Mutex::Autolock _l(mLock);
-        if (effectCreated) {
-            chain->removeEffect_l(effect);
-        }
-        if (effectRegistered) {
-            AudioSystem::unregisterEffect(effectId);
-        }
-        if (chainCreated) {
-            removeEffectChain_l(chain);
+        {
+            Mutex::Autolock _l(mLock);
+            if (effectCreated) {
+                chain->removeEffect_l(effect);
+            }
+            if (effectRegistered) {
+                AudioSystem::unregisterEffect(effect->id());
+            }
+            if (chainCreated) {
+                removeEffectChain_l(chain);
+            }
+
         }
         handle.clear();
     }
