86aaaf98fe87e2e9136a935bef6d514a2e165db8
Adam Seaton <aseaton@google.com>
2017-10-16 17:45:09 +0000

    Revert "Fix memory leaks"
    This reverts commit 12586d1c216c52efbfd2265d240147b1c31fe543.
    
    Change-Id: I3cdb236b5578695d4b0ee6ba2478326e630362d9
    (cherry picked from commit a667247278c732c5317a8fce670c50d42e4a4db0)

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -4636,8 +4636,6 @@ status_t MPEG4Source::read(
         }
         if (size > mBuffer->size()) {
             ALOGE("buffer too small: %zu > %zu", size, mBuffer->size());
-            mBuffer->release();
-            mBuffer = NULL;
             return ERROR_BUFFER_TOO_SMALL;
         }
     }
@@ -4930,8 +4928,6 @@ status_t MPEG4Source::fragmentedRead(
         }
         if (size > mBuffer->size()) {
             ALOGE("buffer too small: %zu > %zu", size, mBuffer->size());
-            mBuffer->release();
-            mBuffer = NULL;
             return ERROR_BUFFER_TOO_SMALL;
         }
     }
