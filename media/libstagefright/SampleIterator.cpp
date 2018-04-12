42bc7f551a692b4bf187fcd4e328e434bc582e0b
sundeepp <sundeepp@nvidia.com>
2018-04-12 15:33:29 +0530

    Stagefright: fix integer overflow
    Fix integer overflow issues in
    SampleIterator.
    
    Change-Id: I0b9bdb2a20ab04ef7bd3fc0e8498cdaec0c3c9fd

--- media/libstagefright/SampleIterator.cpp
+++ media/libstagefright/SampleIterator.cpp
@@ -327,6 +327,10 @@ status_t SampleIterator::findSampleTimeAndDuration(
         ++mTimeToSampleIndex;
     }
 
+    if((mTTSDuration*uint64_t(sampleIndex - mTTSSampleIndex)) > UINT32_MAX) {
+        return ERROR_OUT_OF_RANGE;
+    }
+
     *time = mTTSSampleTime + mTTSDuration * (sampleIndex - mTTSSampleIndex);
 
     int32_t offset = mTable->getCompositionTimeOffset(sampleIndex);
