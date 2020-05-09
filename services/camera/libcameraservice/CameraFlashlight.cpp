b23664c9c5df687221a52272fe917c51204ec9e0
ShubhamB <shubhamprince111@gmail.com>
2020-05-09 13:07:04 +0530

    [SQUASH] Revert for cleaning up av
    Revert "libcameraservice: Don't pass NULL args on setCallbacks call"
    
    This reverts commit 56735d7767e0111dbaf96c137ab554a853fcbbb6.
    
    Change-Id: Ie0c52cce58e163e48cb6180e14eb612b0668e1ef
    
    Revert "Stagefright: Do not skip frames in time-lapse-source for high-speed"
    
    This reverts commit 048d7d78ebfeb25a4d15381cf5eae8058e997c72.
    
    Change-Id: Ic8091bb40421336729f65e2b31d1f19e53dfa1f5
    
    Revert "Stagefright: Allow setting high-framerates in CameraSource"
    
    This reverts commit 742475e11a5d308348fd8a33eb5c9ed08a1ac31e.
    
    Change-Id: I39a8d491924cdb316a2423d4ea3a8f6b4a232af5
    
    Revert "CameraService: Fix deadlock in binder death cleanup."
    
    This reverts commit 2f182036522884e3fea7191b1c9df52ab0ed12dd.
    
    Change-Id: I02a8e618b5a93a682efdff8258e6d5a3205d46ae
    
    Revert "av: compilation fix"
    
    This reverts commit 05a67617a852589e17da94dcd981f3da5c05e4f0.
    
    Change-Id: I7d99f5243815b62825eccd8f8ac590ad5c415fc7
    
    Revert "MTP: Fix crash when no storages are available"
    
    This reverts commit 5e171ca52f18800d800e8fb70bcf4e642f566abd.
    
    Change-Id: I43d20fb8ea4e26ab97907ac0d591f477f7ada1c2
    
    Revert "media: Do not crash on TimeCheck timeout"
    
    This reverts commit befc436c07327a6d75c837dd66c6fa487464f7c3.
    
    Change-Id: I9b14f7da09375e4353b3db45cc425c1f6cdc7463
    
    Change-Id: Ie02068fae3292b17eae976420869e6c8d3a1ab6f

--- services/camera/libcameraservice/CameraFlashlight.cpp
+++ services/camera/libcameraservice/CameraFlashlight.cpp
@@ -512,22 +512,6 @@ status_t CameraHardwareInterfaceFlashControl::initializePreviewWindow(
     return device->setPreviewWindow(mSurface);
 }
 
-static void notifyCallback(int32_t, int32_t, int32_t, void*) {
-    /* Empty */
-}
-
-static void dataCallback(int32_t, const sp<IMemory>&, camera_frame_metadata_t*, void*) {
-    /* Empty */
-}
-
-static void dataCallbackTimestamp(nsecs_t, int32_t, const sp<IMemory>&, void*) {
-    /* Empty */
-}
-
-static void dataCallbackTimestampBatch(int32_t, const std::vector<HandleTimestampMessage>&,
-        void*) {
-    /* Empty */
-}
 status_t CameraHardwareInterfaceFlashControl::connectCameraDevice(
         const String8& cameraId) {
     sp<CameraHardwareInterface> device =
@@ -541,8 +525,7 @@ status_t CameraHardwareInterfaceFlashControl::connectCameraDevice(
     }
 
     // need to set __get_memory in set_callbacks().
-    device->setCallbacks(notifyCallback, dataCallback, dataCallbackTimestamp, 
-            dataCallbackTimestampBatch, this);
+    device->setCallbacks(NULL, NULL, NULL, NULL, NULL);
 
     mParameters = device->getParameters();
 
