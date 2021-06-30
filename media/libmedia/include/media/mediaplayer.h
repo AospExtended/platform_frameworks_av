omnirom/android_frameworks_av

mediaplayer: Add default constructor for media player
CRs-Fixed: 2862424
Change-Id: I639a2086b461f795c749d1dafbf2a3895243b410

media/libmedia/include/media/mediaplayer.h
@@ -207,7 +207,8 @@ class MediaPlayer : public BnMediaPlayerClient,
                     public virtual IMediaDeathNotifier
 {
 public:
-    MediaPlayer(const std::string opPackageName = "");
+    MediaPlayer();
+    explicit MediaPlaye(const std::string opPackageName);
     ~MediaPlayer();
             void            died();
             void            disconnect();

AospExtended/platform_frameworks_av

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

media/libmedia/include/media/mediaplayer.h
@@ -208,7 +208,7 @@ class MediaPlayer : public BnMediaPlayerClient,
 {
 public:
     MediaPlayer();
-    MediaPlayer(const std::string opPackageName);
+    explicit MediaPlayer(const std::string opPackageName);
     ~MediaPlayer();
             void            died();
             void            disconnect();